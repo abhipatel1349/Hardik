3b37b1196d383038f7c7a68b03c64c52c1d255fd
Abhishek Patel <114660447+abhipatel1349@users.noreply.github.com>
2023-03-10 17:09:59 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// This is a simple example of a JavaScript alert
-function showAlert() {
-  alert('Hello World!');
-}
-
-// This is an example of how to add an event listener to a button
-let myButton = document.querySelector('button');
-myButton.addEventListener('click', showAlert);
