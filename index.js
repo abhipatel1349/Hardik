f21709984db3466ea64c68581e954f0b3e9f7782
Abhishek Patel <114660447+abhipatel1349@users.noreply.github.com>
2023-03-10 10:01:37 +0530

    Create index.js
    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// This is a simple example of a JavaScript alert
+function showAlert() {
+  alert('Hello World!');
+}
+
+// This is an example of how to add an event listener to a button
+let myButton = document.querySelector('button');
+myButton.addEventListener('click', showAlert);
